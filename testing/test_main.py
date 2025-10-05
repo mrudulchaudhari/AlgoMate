133f62993b2986ce5b3fdebb466144080b5229a5
Mrudul <mrudulchaudhari0608@gmail.com>
2025-10-05 15:35:02 +0530

    added tests for is_prime and next_prime

--- testing/test_main.py
+++ testing/test_main.py
@@ -18,6 +18,20 @@ class TestMathFunctions(unittest.TestCase):
         self.assertEqual(self.math.fibonacci(5), "The 5th Fibonacci term is 3")
         self.assertEqual(self.math.fibonacci(10), "The 10th Fibonacci term is 34")
 
+    def test_is_prime(self):
+        self.assertEqual(self.math.is_prime(2), "2 is a prime number.")
+        self.assertEqual(self.math.is_prime(3), "3 is a prime number.")
+        self.assertEqual(self.math.is_prime(9), "9 is not a prime number.")
+        self.assertEqual(self.math.is_prime(45), "45 is not a prime number.")
+        self.assertEqual(self.math.is_prime(47), "47 is a prime number.")
+
+    def test_next_prime(self):
+        self.assertEqual(self.math.next_prime(2), "The next prime after 2 is 3")
+        self.assertEqual(self.math.next_prime(3), "The next prime after 3 is 5")
+        self.assertEqual(self.math.next_prime(45), "The next prime after 45 is 47")
+        self.assertEqual(self.math.next_prime(47), "The next prime after 47 is 53")
+        self.assertEqual(self.math.next_prime(99), "The next prime after 99 is 101")
+
 
 if __name__ == '__main__':
     unittest.main()
