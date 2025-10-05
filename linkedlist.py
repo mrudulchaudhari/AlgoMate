9167f043e182477e6056b4c339891a6bc0b6ebcf
Mrudul <mrudulchaudhari0608@gmail.com>
2025-10-05 15:55:14 +0530

    Added code for insert at head, insert at tail, insert before a target

--- linkedlist.py
+++ linkedlist.py
@@ -7,3 +7,36 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
+    def insert_head(self, value):
+        new_node = Node(value)
+        new_node.next = self.head
+        self.head = new_node
+
+    def insert_tail(self, value):
+        new_node = Node(value)
+        if not self.head:
+            self.head = new_node
+            return
+        curr = self.head
+        while curr.next:
+            curr = curr.next
+        curr.next = new_node
+
+    def insert_before(self, target, value):
+        if not self.head:
+            return False
+
+        if self.head.data == target:
+            self.insert_head(value)
+            return True
+
+        prev, curr = None, self.head
+        while curr and curr.data != target:
+            prev, curr = curr, curr.next
+        if curr is None:
+            return False #Target Not Found
+
+        new_node = Node(value)
+        new_node.next = curr
+        prev.next = new_node
+        return True
